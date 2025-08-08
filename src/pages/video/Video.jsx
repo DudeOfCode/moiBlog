390bf8bed839ce7f768464feebc33b1111cca70c
DudeOfCode <usmanmustapha500@gmail.com>
2025-08-08 13:49:45 +0100

    goss

--- src/pages/video/Video.jsx
+++ src/pages/video/Video.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
-import YouTube from 'react-youtube';
+// import YouTube, { YouTubeProps } from 'react-youtube';
 
 const Video = () => {
+
+
     return (
         <div>
-            <div className="shift">
-                <YouTube videoId="2g811Eo7K8U" />
-                {/* <YouTube id="0oRozkpyXQ4" /> */}
-                <YouTube videoId="7k_Z50mhU3I" />
-                <YouTube videoId="0oRozkpyXQ4" />
-            </div>
+            <div>
 
+                {/* <YouTube videoId="2g811Eo7K8U" /> */}
+                {/* <YouTube id="0oRozkpyXQ4" /> */}
+                {/* <YouTube videoId="7k_Z50mhU3I" />
+                <YouTube videoId="0oRozkpyXQ4" /> */}
+            </div>
         </div>
     )
 }
