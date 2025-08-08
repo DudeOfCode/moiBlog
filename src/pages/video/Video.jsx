6fdb20c924f564fe04d537e05054502840da7e19
DudeOfCode <usmanmustapha500@gmail.com>
2025-08-08 14:52:45 +0100

    vomet

--- src/pages/video/Video.jsx
+++ src/pages/video/Video.jsx
@@ -14,54 +14,129 @@ import {
     MediaMuteButton,
     MediaFullscreenButton,
 } from "media-chrome/react";
-
+import "./Video.css"
 
 const Video = () => {
 
 
     return (
-        <div>
-            <div>
-               
+        <div className="off-lie">
+            <div className="shift">
+
                 {/* <YouTube videoId="2g811Eo7K8U" /> */}
                 {/* <YouTube id="0oRozkpyXQ4" /> */}
                 {/* <YouTube videoId="7k_Z50mhU3I" />
                 <YouTube videoId="0oRozkpyXQ4" /> */}
+                <div>
+                    <MediaController
 
-                <MediaController
-                    style={{
-                        width: "100%",
-                        aspectRatio: "16/9",
-                    }}
-                >
-                    <ReactPlayer
-                        slot="media"
-                        src="https://stream.mux.com/maVbJv2GSYNRgS02kPXOOGdJMWGU1mkA019ZUjYE7VU7k"
-                        controls={false}
                         style={{
-                            width: "100%",
-                            height: "100%",
-                            "--controls": "none",
+                            width: "70%",
+                            aspectRatio: "16/9",
+                            marginLeft: "30vh",
                         }}
-                    ></ReactPlayer>
-                    <MediaControlBar>
-                        <MediaPlayButton />
-                        <MediaSeekBackwardButton seekOffset={10} />
-                        <MediaSeekForwardButton seekOffset={10} />
-                        <MediaTimeRange />
-                        <MediaTimeDisplay showDuration />
-                        <MediaMuteButton />
-                        <MediaVolumeRange />
-                        <MediaPlaybackRateButton />
-                        <MediaFullscreenButton />
-                    </MediaControlBar>
-                </MediaController>
+                    >
+                        <ReactPlayer
+                            slot="media"
+                            src="https://stream.mux.com/maVbJv2GSYNRgS02kPXOOGdJMWGU1mkA019ZUjYE7VU7k"
+                            controls={false}
+                            style={{
+                                width: "100%",
+                                height: "100%",
+                                "--controls": "none",
+                            }}
+                        ></ReactPlayer>
+                        <MediaControlBar>
+                            <MediaPlayButton />
+                            <MediaSeekBackwardButton seekOffset={10} />
+                            <MediaSeekForwardButton seekOffset={10} />
+                            <MediaTimeRange />
+                            <MediaTimeDisplay showDuration />
+                            <MediaMuteButton />
+                            <MediaVolumeRange />
+                            <MediaPlaybackRateButton />
+                            <MediaFullscreenButton />
+                        </MediaControlBar>
+                    </MediaController>
+                </div>
 
-                <ReactPlayer style={{
-                    width: "100%",
-                    height: "100%",
-                    "--controls": "none",
-                }} src='https://www.youtube.com/watch?v=LXb3EKWsInQ' controls />
+                <div className='small-vid'>
+                    <div>
+                        <ReactPlayer style={{
+                            width: "20%",
+                            height: "auto",
+                            "--controls": "none",
+                        }} src='https://www.youtube.com/watch?v=LXb3EKWsInQ' controls />
+                    </div>
+                    <span>
+                        <ReactPlayer style={{
+                            width: "20%",
+                            height: "auto",
+                            "--controls": "none",
+                        }} src='https://www.youtube.com/watch?v=LXb3EKWsInQ' controls />
+                    </span>
+                    <span>
+                        <ReactPlayer style={{
+                            width: "20%",
+                            height: "auto",
+                            "--controls": "none",
+                        }} src='https://www.youtube.com/watch?v=LXb3EKWsInQ' controls />
+                    </span>
+
+
+
+                </div>
+                <div className='small-vid'>
+                    <div>
+                        <ReactPlayer style={{
+                            width: "20%",
+                            height: "auto",
+                            "--controls": "none",
+                        }} src='https://www.youtube.com/watch?v=LXb3EKWsInQ' controls />
+                    </div>
+                    <span>
+                        <ReactPlayer style={{
+                            width: "20%",
+                            height: "auto",
+                            "--controls": "none",
+                        }} src='https://www.youtube.com/watch?v=LXb3EKWsInQ' controls />
+                    </span>
+                    <span>
+                        <ReactPlayer style={{
+                            width: "20%",
+                            height: "auto",
+                            "--controls": "none",
+                        }} src='https://www.youtube.com/watch?v=LXb3EKWsInQ' controls />
+                    </span>
+
+
+
+                </div>
+                <div className='small-vid'>
+                    <div>
+                        <ReactPlayer style={{
+                            width: "20%",
+                            height: "auto",
+                            "--controls": "none",
+                        }} src='https://www.youtube.com/watch?v=LXb3EKWsInQ' controls />
+                    </div>
+                    <span>
+                        <ReactPlayer style={{
+                            width: "20%",
+                            height: "auto",
+                            "--controls": "none",
+                        }} src='https://www.youtube.com/watch?v=LXb3EKWsInQ' controls />
+                    </span>
+                    <span>
+                        <ReactPlayer style={{
+                            width: "20%",
+                            height: "auto",
+                            "--controls": "none",
+                        }} src='https://www.youtube.com/watch?v=LXb3EKWsInQ' controls />
+                    </span>
+
+                </div>
+
             </div>
         </div>
     )
